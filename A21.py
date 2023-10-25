10672cd69f0ca25b803e1ee089756866c8365ec3
Kyuwoong Lee <kwooler@gmail.com>
2023-10-25 12:13:02 -0700

    Create A21.py

---
+++ A21.py
@@ -0,0 +1,27 @@
+def main():
+    """
+    ##################################################
+    Comlete your code here
+    Use m_perc and f_perc for your results
+    ##################################################
+    """
+    mnum = int(input('Enter the number of male students: '))
+    fnum = int(input('Enter the number of female students: '))
+    
+    total = fnum + mnum
+    m_perc = mnum / total * 100
+    f_perc = fnum / total * 100
+    print (f'The percentage of female student is (f_perc:.2f)')
+    print (f'The percentage of male student is (m_perc:.2f)')
+
+
+    """
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    """
+    return m_perc, f_perc
+
+
+if __name__ == '__main__':
+    main()
