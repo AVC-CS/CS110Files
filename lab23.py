1105fa435d1998b5c78be64e9a4d1bd246655baf
Kyuwoong Lee <kwooler@gmail.com>
2023-10-25 12:07:29 -0700

    Create lab23.py

---
+++ lab23.py
@@ -0,0 +1,26 @@
+def main():
+    ##################################################
+    # Comlete your code here
+    ##################################################
+    """
+    Use following variables to save your result
+    total 
+    average
+    """
+    val1 = int(input('Enter your number '))
+    val2 = int(input('Enter your number '))
+    val3 = int(input('Enter your number '))
+    total = val1 + val2 + vale
+    average = total / 3
+
+    print (f'The total is {total}')
+    print (f'The average is {average:.2f}')
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return total, average
+
+
+if __name__ == '__main__':
+    main()
