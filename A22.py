f50ebac173abaa3cf707ccddbe05c84b5281a0f9
Kyuwoong Lee <kwooler@gmail.com>
2023-10-25 12:14:08 -0700

    Create A22.py

---
+++ A22.py
@@ -0,0 +1,22 @@
+def main():
+    """
+    ##################################################
+    # Comlete your code here
+    Use the same variables: celsius fahrenheit 
+    ##################################################
+    """
+    celsius = int(input('Enter yout tempeture in Celsius: '))
+    fahrenheit = 9 / 5 * celsius + 32
+
+    print (f'The converted temperature is {fahrenheit:.2f}')
+
+    """
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    """
+    return celsius, fahrenheit
+
+
+if __name__ == '__main__':
+    main()
